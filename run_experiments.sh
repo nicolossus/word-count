31ceb218f89f234213596fc741868d878a17e245
Nicolai Haug <nicolaihaug@yahoo.com>
2024-03-19 16:56:11 +0100

    Specify python executer version

--- run_experiments.sh
+++ run_experiments.sh
@@ -16,6 +16,6 @@ mkdir -p ${RESULTSDIR}
 mkdir -p ${FIGDIR}
 
 for title in abyss isles last sierra; do
-    python code/count.py ${DATADIR}/${title}.txt -o ${RESULTSDIR}/${title}.json
-    python code/plot.py ${RESULTSDIR}/${title}.json -o ${FIGDIR}/${title}.png
+    python3 code/count.py ${DATADIR}/${title}.txt -o ${RESULTSDIR}/${title}.json
+    python3 code/plot.py ${RESULTSDIR}/${title}.json -o ${FIGDIR}/${title}.png
 done
